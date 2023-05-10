6f67fa0f86a657533c84a3e536a98c9a0c744faa
Zohaib234 <zhbah234@gmail.com>
2023-05-10 11:03:49 +0500

    some important widget code is added like list tile and list view builder

--- lib/main.dart
+++ lib/main.dart
@@ -17,63 +17,132 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+          children:  [
 
+              // SizedBox(height: 20),
 
-            // lets learn about expended widget . this widget is used to occupy all the space on the scree
+             
 
-            Row(
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context,index){
+                  
+                  return const  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://c8.alamy.com/comp/HAG2XK/man-hand-holding-two-gun-and-ready-to-shooting-walking-zombie-to-him-HAG2XK.jpg"),
 
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 200,
-                    color: Colors.orange,
-                    child:  const  Center(child: Text("container 1")),
-                  ),
-                ),
-                 // const SizedBox(width: 10,),
-                Expanded(
-                  child: Container(
-                    height: 200,
-                    color: Colors.teal,
-                    child:  const  Center(child: Text("container 2")),
-                  ),
-                ),
-              ],
-            ),
+                    ),
+                    title: Text("Zohaib"),
+                    subtitle: Text(" i have Zombie mentality "),
+                    trailing: Text("2:30 PM"),
 
-            const  SizedBox(height: 20),
+                  );
+                }
 
-
-            // this code is all about the container and its properties
-            Center(
-              child: Container(
-                height: 250,
-                width: 250,
-                transform: Matrix4.rotationZ(0.2),
-                decoration: BoxDecoration(
-                    color: Colors.orangeAccent,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.red, width: 01),
-                    image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            "https://www.w3schools.com/w3images/avatar2.png")),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.deepPurpleAccent, blurRadius: 100),
-                    ]),
-                child: const Center(child: Text("Zohaib Khoso")),
               ),
             ),
-
-
-
-
           ],
         ),
       ),
     );
   }
 }
+
+// Text Form Field code
+//
+// Padding(
+// padding: const EdgeInsets.all(26.0),
+// child: TextFormField(
+// keyboardType: TextInputType.emailAddress,
+//
+// decoration:   InputDecoration(
+// prefixIcon: const  Icon(Icons.email_outlined),
+// hintText: "Enter your mail address",
+// fillColor: Colors.grey,
+// label: const  Text("Email"),
+// hintStyle: const  TextStyle(
+// color: Colors.black,
+//
+// ),
+// enabledBorder:  OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10)
+// ),
+// focusedBorder:   OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10)
+// ),
+// ),
+//
+// ),
+// ),
+
+// lets talk about circular avattar
+
+// const Center(
+//   child:  CircleAvatar(
+//     radius: 20,
+//     backgroundImage: NetworkImage("https://images.pexels.com/photos/15167470/pexels-photo-15167470.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") ,
+//
+//   ),
+// ),
+// const  Divider(thickness: 2, color: Colors.orange,),
+
+// const  SizedBox(height: 50,
+// child: VerticalDivider(color: Colors.orange, thickness: 2,),),
+
+// lets learn about expended widget . this widget is used to occupy all the space on the scree
+
+// Row(
+//   children: [
+//     Expanded(
+//       child: Container(
+//         height: 50,
+//         color: Colors.orange,
+//         child: const Center(child: Text("container 1")),
+//       ),
+//     ),
+//     // const SizedBox(width: 10,),
+//     Expanded(
+//       child: Container(
+//         height: 50,
+//         color: Colors.teal,
+//         child: const Center(child: Text("container 2")),
+//       ),
+//     ),
+//   ],
+// ),
+
+// const SizedBox(height: 20),
+
+// this code is all about the container and its properties
+// Center(
+//   child: Container(
+//     height: 50,
+//     width: 50,
+//     decoration: BoxDecoration(
+//         color: Colors.orangeAccent,
+//         borderRadius: BorderRadius.circular(10),
+//         border: Border.all(color: Colors.red, width: 01),
+//         image: const DecorationImage(
+//             fit: BoxFit.cover,
+//             image: NetworkImage(
+//                 "https://www.w3schools.com/w3images/avatar2.png")),
+//         boxShadow: const [
+//           BoxShadow(
+//               color: Colors.deepPurpleAccent, blurRadius: 100),
+//         ]),
+//     child: const Center(child: Text("Zohaib Khoso")),
+//   ),
+// ),
+// const  SizedBox(height: 20),
+// RichText(
+//   text:  const TextSpan(
+//     text: 'Hello ',
+//     style: TextStyle(color: Colors.black),
+//     children:  <TextSpan>[
+//       TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold , color: Colors.black)),
+//       TextSpan(text: ' world!'),
+//     ],
+//   ),
+// ),
+
